faf3ebe1229f1ba93d776395a57df4776454cd2c
xujiawei <260389174@qq.com>
2015-08-13 18:43:22 +0800

    Signed-off-by: xujiawei <260389174@qq.com>

--- database/src/tmp/RiseTest.java
+++ database/src/tmp/RiseTest.java
@@ -8,11 +8,11 @@ public class RiseTest {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		String requestUrl = "http://hq.sinajs.cn/list=sz150210";
-		while (true) {
-			System.out.println(HttpConnection.requestForGet(requestUrl, "gbk"));
-			Thread.sleep(2000);
-		}
+		String requestUrl = "http://finance.sina.com.cn/data/";
+		// while (true) {
+		System.out.print(HttpConnection.requestForGet(requestUrl, "gbk"));
+		// Thread.sleep(2000);
+		// }
 
 	}
 
